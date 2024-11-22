a38e64115cd177fcee24d9501a494e0d5e44aae6
Alexey Rusaleev <alexey.rusaleev@yandex.ru>
2024-11-22 19:00:35 +0700

    day 1 part 1. tests are passed green but real input makes it fail

--- 1/solvepuzzle.py
+++ 1/solvepuzzle.py
@@ -1,9 +1,24 @@
 
 def solve(puzzle):
-    return 0
+    start_floor = 0
+    end_floor = inefficient_recursive(puzzle, start_floor)
+    return end_floor
+
+def inefficient_recursive(puzzle, floor):
+    if puzzle == '':
+        return floor
+    else:
+        if puzzle[0] == '(':
+            floor += 1
+        if puzzle[0] == ')':
+            floor -= 1
+        the_rest_of_puzzle = puzzle[1:]
+        return inefficient_recursive(the_rest_of_puzzle, floor)
 
 def main():
-    print(solve(input()))
+    with open('input.txt') as file:
+        puzzle = file.read()
+        print(solve(puzzle))
 
 if __name__ == '__main__':
     main()
